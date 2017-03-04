fee079c453b0b37ccaf5abb1eb3c2175f6221a62
learntofreedom <learntofeedom@gmail.com>
2017-03-04 17:09:43 +0800

    implement product show

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -13,6 +13,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
+
   process resize_to_fit: [800, 800]
 
      version :thumb do

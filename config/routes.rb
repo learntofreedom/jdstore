f9708740e861b5d21885e8d1b3e9e289643eb9cd
learntofreedom <learntofeedom@gmail.com>
2017-03-05 15:14:34 +0800

    implement carts index function

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@ Rails.application.routes.draw do
   devise_for :users
   namespace :admin do
   resources :products
- end
 
+ end
+resources :carts
  resources :products do
     member do
     post :add_to_cart
